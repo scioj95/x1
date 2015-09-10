caae98077070344a74015ccef5047d7f180db55e
scioj95 <josephsciorilli@yahoo.com>
2015-09-09 21:19:40 -0400

    Update x1.java

--- x1.java
+++ x1.java
@@ -19,17 +19,23 @@ void setup() {
 //// NEXT FRAME:  scene, action, show.
 void draw() {
   //// SCENE:  sky, sun, tree, house, etc.
-  background( 100,150,200 );                // sky
+  background( 100,200,250 );                // sky
   fill( 255,255,0 );
-  ellipse( width*3/4, height/8, 40,40 );    // sun
+  ellipse( width*1/8, height/4, 40,40 );    // sun
   // Grass
   fill( 100,200,100 );
   rect( 0,horizon, width,height*3/4 );      // grass.
   
   /* INSERT YOUR CODE HERE! */
-  triangle( 150,horizon, 120,horizon-50, 180,horizon-50  );  // tree
-  text( "This is NOT a good tree; please fix it!", 150,horizon );
-                                            // house
+  triangle( 500,horizon-25, 550,horizon-25, 525,horizon-75  );  // tree
+  noStroke();
+  triangle( 500,horizon-15, 550,horizon-15, 525,horizon-75  );
+  fill(130, 42, 42 );
+  rect(518, horizon-15, 15, 15);
+  fill(0);
+  text( "Now it is a good tree!!", 522,horizon -100 );
+  fill(0, 160,  );
+  rect(width*1/4, horizon-50, 50, 50);                                          // house
 
   fill(0);
   text( "My name is Mud", 10,height-20 );                                          
